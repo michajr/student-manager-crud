625a481118e406eddf03e14fb36d2f0f814359dd
Michael Campusano <michael@gmail.com>
2024-02-13 15:04:27 -0400

    Optimizing the AppApplication.java

--- app/src/main/java/com/thymeleaf/app/AppApplication.java
+++ app/src/main/java/com/thymeleaf/app/AppApplication.java
@@ -1,11 +1,7 @@
 package com.thymeleaf.app;
 
-import com.thymeleaf.app.models.Student;
-import com.thymeleaf.app.repository.StudentRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
@@ -14,18 +10,4 @@ public class AppApplication {
     SpringApplication.run(AppApplication.class, args);
   }
 
-  @Bean
-  public CommandLineRunner commandLineRunner(
-    StudentRepository studentRepository
-  ) {
-    return args -> {
-      Student student = new Student();
-      student.setId(1);
-      student.setName("Michael");
-      student.setLastName("Campusano");
-      student.setEmail("mc@gmail.com");
-
-      studentRepository.save(student);
-    };
-  }
 }
